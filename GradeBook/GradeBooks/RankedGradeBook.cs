89289e18bff86524532e618a8937d130603a4840
Rianne van der Linden <Rianne.van.der.Linden@rabobank.nl>
2021-02-26 14:37:44 +0100

    Feat(Type): set GradeBookType to Ranked

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,9 +1,12 @@
-﻿namespace GradeBook.GradeBooks
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
+            Type = GradeBookType.Ranked;
         }
     }
 }
